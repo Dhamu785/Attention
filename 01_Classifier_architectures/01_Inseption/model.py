1fa09b244d4642e608b83ade0728e6e0b32c0ae3
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-04-07 13:21:25 +0530

    workflow

--- 01_Classifier_architectures/01_Inseption/model.py
+++ 01_Classifier_architectures/01_Inseption/model.py
@@ -1,3 +1,13 @@
 import torch as t
 import torch.nn as nn
 
+'''
+    01. Architectures / Depthwise seperable NN
+    02. Attention
+    03. Channel attention
+    04. Spatial attention
+    05. Self attention
+    06. CBAM
+    07. Attention in unet
+    08. Transformers
+'''
