93151d2f3daed7bb70946afda19f12c5d09fed62
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-04-17 19:56:39 +0530

    variables initated in init fun

--- 01_Classifier_architectures/01_Inseption/googleNet.py
+++ 01_Classifier_architectures/01_Inseption/googleNet.py
@@ -5,3 +5,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from typing import Optional, List, Callable, Any, Union, Tuple
+import warnings
+
+class Inception(nn.Module):
+    def __init__(self, num_classes: int, aux_logit: bool, transform_input: bool,
+                    init_weight: Optional[bool] = None, blocks: List[Callable[..., nn.Module]] = None,
+                    drop_out: float = 0.20, drop_out_aux: float = 0.70) -> None:
+        super().__init__()
+        if init_weight is None:
+            warnings.warn("The score behaviours are different from the original GoogleNet", FutureWarning)
+            init_weight = True
+        if len(blocks) != 3:
+            raise ValueError(f"Blocks length should be 3 instead of {len(blocks)}")
+        conv_block = blocks[0]
+        inception_block = blocks[1]
+        inception_aux_block = blocks[2]
+
+        self.aux_logit = aux_logit
+        self.transform_input = transform_input
+        
+        
