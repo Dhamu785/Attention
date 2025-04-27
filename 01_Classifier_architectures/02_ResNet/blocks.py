0a5077da6c2e735dee36bdf33f3989617a6a0b11
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-04-28 00:25:10 +0530

    BasicBlock added (Residual block)

--- 01_Classifier_architectures/02_ResNet/blocks.py
+++ 01_Classifier_architectures/02_ResNet/blocks.py
@@ -2,7 +2,7 @@ import torch as t
 import torch.nn as nn
 from typing import Tuple, Union, Callable, Optional
 
-def conv1x1(in_planes: int, out_planes: int, strides: int=1, groups: int=1, dilation: int=1) -> nn.Module:
+def conv3x3(in_planes: int, out_planes: int, strides: int=1, groups: int=1, dilation: int=1) -> nn.Module:
     return nn.conv2d(in_channels=in_planes, out_channels=out_planes, kernel_size=(3,3), stride=strides,
                         padding=dilation, groups=groups, bias=False, dilation=dilation)
 
@@ -13,4 +13,29 @@ class BasicBlock(nn.Module):
     expansion: int=1
 
     def __init__(self, in_planes: int, planes: int, stride: int=1, downsample: Optional[Callable[..., nn.Module]]=None, norm_layer: Optional[Callable[..., nn.Module]]=None) -> None:
-        ...
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self.conv1 = conv3x3(in_planes=in_planes, out_planes=planes, strides=stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(in_planes=in_planes, out_planes=planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identy = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        # out = self.relu(out)
+
+        if self.downsample is not None:
+            identy = self.downsample(out)
+        
+        out += identy
+        out = self.relu(out)
+
+        return out
