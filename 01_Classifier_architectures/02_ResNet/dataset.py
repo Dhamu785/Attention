d3b8f0ce7e33d39acd216f2dc648db3cfb10b81c
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-05-04 01:22:20 +0530

    spelling error

--- 01_Classifier_architectures/02_ResNet/dataset.py
+++ 01_Classifier_architectures/02_ResNet/dataset.py
@@ -27,7 +27,7 @@ class custom_data_prep(Dataset):
         for cls_n in self.classes:
             fldr_fls = os.listdir(os.path.join(root_dir, cls_n))
             for file in fldr_fls:
-                self.img_paths.appedn(os.path.join(root_dir, cls_n, file))
+                self.img_paths.append(os.path.join(root_dir, cls_n, file))
                 self.labels.append(self.cls_to_idx[cls_n])
 
         def __len__(self) -> int:
