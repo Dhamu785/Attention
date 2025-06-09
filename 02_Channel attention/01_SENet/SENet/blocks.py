b0557055f65255c7835dc53f142b3e0ef1215db5
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-06-09 14:12:21 +0530

    update

--- 02_Channel attention/01_SENet/SENet/blocks.py
+++ 02_Channel attention/01_SENet/SENet/blocks.py
@@ -2,7 +2,15 @@ import torch as t
 from torch import nn
 from typing import Optional, Callable, Union
 
-class conv3x3_block(nn.Module):
-    def __init__(self, in_chennels: int, out_chennels: int, kernel_size: int, stride: int, padding: int, dilation: int = 1, groups: int = 1, bias: bool = False,
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, dilation: int, groups: int, 
+                    bias: bool, bn_eps: float = 1e-5, activation: Union[Callable[..., t.Tensor], str] = (lambda: nn.ReLU(inplace=True))) -> None:
+        super().__init__()
+
+        self.conv = nn.Conv2d()
+
+
+def conv3x3_block(in_chennels: int, out_chennels: int, stride: int, padding: int, dilation: int = 1, groups: int = 1, bias: bool = False,
                     bn_eps: float = 1e-5, activation: Union[Callable[..., t.Tensor], str]=(lambda: nn.ReLU(inplace=True))):
-        ...
+
+        self.conv = nn.Conv2d(in_channels=in_chennels, out_channels=out_chennels)
