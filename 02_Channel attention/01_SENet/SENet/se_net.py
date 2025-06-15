525406a65c3b41477e774dd3b23ada9c51c926a4
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-06-15 23:00:31 +0530

    local variable changed

--- 02_Channel attention/01_SENet/SENet/se_net.py
+++ 02_Channel attention/01_SENet/SENet/se_net.py
@@ -41,7 +41,6 @@ class bottleneck(nn.Module):
         return self.conv3(self.conv2(self.conv1(x)))
 
 class SEblock(nn.Module):
-    sav = 0
     def __init__(self, channels: int, reduction: int = 16, approx_sigmoid: bool = False, 
                     activation: Optional[Union[Callable[..., t.Tensor], str]] = (lambda: nn.ReLU(inplace=True))) -> None:
         super().__init__()
@@ -52,14 +51,15 @@ class SEblock(nn.Module):
         self.activ = get_activation(activation)
         self.conv2 = nn.Conv2d(in_channels=mid_channels, out_channels=channels, kernel_size=1, stride=1, groups=1, bias=False)
         self.sigmoid = nn.Sigmoid() if approx_sigmoid else nn.Sigmoid()
+        self.sav = 0
 
     def forward(self, x: t.Tensor) -> t.Tensor:
         X = x * self.sigmoid(self.conv2(self.activ(self.conv1(self.pool(x)))))
         if not self.training and len(x) == 6:
             timestamp = datetime.now().strftime("%H%M%S")
-            sav+=1
-            np.save(f'./seIO/Inp_{timestamp}_{sav}.npy', x.detach().cpu().numpy())
-            np.save(f'./seIO/Outp_{timestamp}_{sav}.npy', X.detach().cpu().numpy())
+            self.sav+=1
+            np.save(f'./seIO/Inp_{timestamp}_{self.sav}.npy', x.detach().cpu().numpy())
+            np.save(f'./seIO/Outp_{timestamp}_{self.sav}.npy', X.detach().cpu().numpy())
         return X
 
 class SENetUnit(nn.Module):
