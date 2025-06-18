f5cf6ac56f41eccec55777df0c3628dc6149b476
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-06-18 19:44:10 +0530

    CAM init

--- 03_Spatial attention (CBAM)/CBAM.py
+++ 03_Spatial attention (CBAM)/CBAM.py
@@ -4,11 +4,24 @@ import torch.nn as nn
 class ConvBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, **kwargs) -> None:
         super().__init__()
-        self.conv = nn.Sequential([
+        self.conv = nn.Sequential(
             nn.Conv2d(in_channels=in_channels, out_channels=out_channels, **kwargs),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
-        ])
+        )
 
     def forward(self, x: t.Tensor) -> t.Tensor:
         return self.conv(x)
+    
+class CAM(nn.Module):
+    def __init__(self, in_channels: int, reduction_factor: int) -> None:
+        super().__init__()
+        self.channels = in_channels
+        self.reduction = reduction_factor
+        self.avg = nn.AdaptiveAvgPool2d(output_size=1)
+        self.max = nn.AdaptiveMaxPool2d(output_size=1)
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=self.channels, out_features=self.channels//self.reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=self.channels//self.reduction, out_features=self.channels)
+        )
